38797e34d763bb7198fc943293e01e744f656d36
sym-uma-suki <syouma1240@icloud.com>
2025-08-30 20:10:37 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,5 @@
+const CACHE = 'bousai-v1';
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('bousai-v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/app.js',
-        '/manifest.json'
-      ]);
-    })
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/','/index.html','/style.css','/app.js','/manifest.json'])));
 });
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
-});
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
